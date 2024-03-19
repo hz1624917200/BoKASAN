5efdeaee6e0df7108e20b2c96d5b2ae76ee76190
Zheng <hz1624917200@outlook.com>
2024-03-19 11:03:47 +0800

    fix: improve debug print

--- src/alloc.c
+++ src/alloc.c
@@ -514,9 +514,10 @@ bool alloc_shadow(size_t size, unsigned long addr){
 		if (irq_count()) {
 			printk("BoKASAN: !!!!!!!!!!IRQ COUNT IN alloc_shadow!!!!!!!!!!!!\n");
 		}
-		printk("BoKASAN: !!!!!!!!!!PID %u NOT PRESENT IN alloc_shadow!!!!!!!!!!!!\n", current->pid);
 		if (is_vaddr_special(addr)) {
 			printk("BoKASAN: SPECIAL ADDR %lx IN alloc_shadow\n", addr);
+		} else {
+			printk("BoKASAN: !!!!!!!!!!PID %u NOT PRESENT IN alloc_shadow!!!!!!!!!!!!\n", current->pid);
 		}
 		dump_stack();
 	}
